8a33d1cc355a8bd1d221f60bb58930d292827799
andyeschultheiss <61433811+andyeschultheiss@users.noreply.github.com>
2020-05-06 15:08:54 -0700

    Daily Report exercise
    Simple console dialog recreating daily report questions from Tech Academy LMS

---
+++ Tech-Academy-Daily-Report/Tech-Academy-Daily-Report/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Tech_Academy_Daily_Report
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+
+            Console.WriteLine("What course are you on?");
+            string currentCourse = Console.ReadLine();
+
+            Console.WriteLine("Which page number?");
+            int currentPage = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\".");
+            bool needsHelp = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            string posExperiences = Console.ReadLine();
+
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            string miscFeedback = Console.ReadLine();
+
+            Console.WriteLine("How many hours did you study today?");
+            int hoursStudied = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Variables-and-Data-Types/Variables-and-Data-Types/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Variables_and_Data_Types
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + yourName);
+            //Console.ReadLine();
+
+            Console.WriteLine("What is your favorite number?");
+            int yourNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your favorite number is: " + yourNumber);
+            Console.ReadLine();
+        }
+    }
+}
