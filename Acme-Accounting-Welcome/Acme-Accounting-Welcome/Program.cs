9fd1150b920f2ea968762512b515efa7fedc7beb
andyeschultheiss <61433811+andyeschultheiss@users.noreply.github.com>
2020-05-06 11:05:33 -0700

    Initial commit
    Acme-Accounting-Welcome is a demo of a console app with text output

---
+++ Acme-Accounting-Welcome/Acme-Accounting-Welcome/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Acme_Accounting_Welcome
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we’re \"accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
