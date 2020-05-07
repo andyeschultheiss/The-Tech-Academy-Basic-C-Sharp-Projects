728eea0c3611ecd698b748f702a41213a23d457e
andyeschultheiss <61433811+andyeschultheiss@users.noreply.github.com>
2020-05-07 14:18:51 -0700

    Insurance questionnaire
    Simple use of boolean operators

---
+++ Car-Insurance-Questionnaire/Car-Insurance-Questionnaire/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Car_Insurance_Questionnaire
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to the Car Insurance Questionnaire.");
+            Console.WriteLine("Answer just 3 questions and we can tell you if you qualify for insurance.");
+
+            Console.WriteLine("Q1. What is your age?");
+            ushort customerAge = Convert.ToUInt16(Console.ReadLine());
+
+            Console.WriteLine("Q2. Have you ever had a DUI? Please answer true/false");
+            bool hasDUI = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("Q3. How many speeding tickets do you have?");
+            ushort numberOfTickets = Convert.ToUInt16(Console.ReadLine());
+
+            Console.WriteLine("Qualified?");
+            Console.WriteLine((customerAge > 15) && !(hasDUI) && (numberOfTickets <= 3));
+
+            Console.ReadLine();
+        }
+    }
+}
