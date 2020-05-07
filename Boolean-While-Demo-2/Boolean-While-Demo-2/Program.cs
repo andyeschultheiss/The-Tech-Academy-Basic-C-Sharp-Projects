cddf586a221df19a54e8cb459ec4f6f0f9771c4f
andyeschultheiss <61433811+andyeschultheiss@users.noreply.github.com>
2020-05-07 15:40:56 -0700

    While demos
    demo usage of while and do-while loops

---
+++ Boolean-While-Demo-2/Boolean-While-Demo-2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Boolean_While_Demo_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to the Guess-the-Number Game!");
+            int inputNum = 0;
+
+            while (inputNum != 42)
+            {
+                Console.WriteLine("Please enter an integer of your choice:");
+                inputNum = Convert.ToInt32(Console.ReadLine());
+                if (inputNum == 42)
+                {
+                    Console.WriteLine("Correct! You guessed the number.");
+                    break;
+                }
+                string suggestion = inputNum > 42 ? "Too high. Try a lower number." : "Too low. Try a greater number.";
+                Console.WriteLine(suggestion);
+
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ Boolean-While-Demo/Boolean-While-Demo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Boolean_While_Demo
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to the Guess-the-Number Game!");
+            int inputNum = 0;
+            do
+            {
+                Console.WriteLine("Please enter an integer of your choice:");
+                inputNum = Convert.ToInt32(Console.ReadLine());
+                if (inputNum == 7)
+                {
+                    Console.WriteLine("Correct! You guessed the number.");
+                    break;
+                }
+                string suggestion = inputNum > 7 ? "Too high. Try a lower number." : "Too low. Try a greater number.";
+                Console.WriteLine(suggestion);
+
+            }
+            while (inputNum != 7);
+
+            Console.Read();
+
+        }
+    }
+}
