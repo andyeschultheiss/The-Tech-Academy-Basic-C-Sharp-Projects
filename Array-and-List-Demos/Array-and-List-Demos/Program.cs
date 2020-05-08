b56e6736ab27a10761ddec44ef88f269c365942b
andyeschultheiss <61433811+andyeschultheiss@users.noreply.github.com>
2020-05-08 11:34:01 -0700

    Array and List Demo
    take user input and return array and list values

---
+++ Array-and-List-Demos/Array-and-List-Demos/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        //string array with user selection of index
+        string[] nameArray = { "Frodo", "Samwise", "Merry", "Pippin", "Aragorn", "Legolas", "Gimli", "Boromir", "Gandalf" };
+        int userSelection = -1;  //trigger while loop entry
+        while (userSelection < 0 || userSelection > 8)
+        {
+            Console.WriteLine("Please select an index number (0-8) to display the name at that index.");
+            userSelection = Convert.ToInt32(Console.ReadLine());
+            if (userSelection < 0 || userSelection > 8)
+            {
+                Console.WriteLine("There is no index " + userSelection + " in the sample array.");
+            }
+        }
+        Console.WriteLine("The name at that index is: " + nameArray[userSelection]);
+
+        //integer array with user selection of index
+        int[] numberArray = { 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 };
+        userSelection = -1;  //trigger while loop entry
+        while (userSelection < 0 || userSelection > 9)
+        {
+            Console.WriteLine("Please select an index number (0-9) to display the number at that index.");
+            userSelection = Convert.ToInt32(Console.ReadLine());
+            if (userSelection < 0 || userSelection > 9)
+            {
+                Console.WriteLine("There is no index " + userSelection + " in the sample array.");
+            }
+        }
+        Console.WriteLine("The number at that index is: " + numberArray[userSelection]);
+        
+        //List of strings
+        List<string> nameList = new List<string>() { "Frodo", "Samwise", "Merry", "Pippin", "Aragorn", "Legolas", "Gimli", "Boromir", "Gandalf" };
+        userSelection = -1;  //trigger while loop entry
+        while (userSelection < 0 || userSelection > 8)
+        {
+            Console.WriteLine("Please select an index number (0-8) to display the name at that index.");
+            userSelection = Convert.ToInt32(Console.ReadLine());
+            if (userSelection < 0 || userSelection > 8)
+            {
+                Console.WriteLine("There is no index " + userSelection + " in the sample list.");
+            }
+        }
+        Console.WriteLine("The name at that index is: " + nameList[userSelection]);
+
+        Console.ReadLine();
+        //
+    }
+
+}
---
+++ Arrays-and-Lists/Arrays-and-Lists/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main()
+    {
+        // typically use arrays for a fixed size
+        int[] numberArray = new int[5];
+        numberArray[0] = 5;
+        numberArray[1] = 10;
+        numberArray[2] = 15;
+        numberArray[3] = 20;
+        numberArray[4] = 25;
+        Console.WriteLine(numberArray[4]);
+
+        int[] numberArray1 = new int[] { 1, 2, 3, 4, 5 };
+        Console.WriteLine(numberArray1[2]);
+
+        int[] numberArray2 = { 3, 6, 9, 12 };
+        Console.WriteLine(numberArray2[2]);
+
+        List<int> intList = new List<int>();
+        intList.Add(1);
+        intList.Add(2);
+        intList.Add(3);
+        intList.Add(4);
+        intList.Remove(4);
+        Console.WriteLine(intList[1]);
+
+        Console.ReadLine();
+
+
+    }
+}
