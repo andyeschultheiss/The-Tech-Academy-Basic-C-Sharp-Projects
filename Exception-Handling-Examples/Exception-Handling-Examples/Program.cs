b5f45ba3c68590629f74154d59d2398b9fe09a6e
andyeschultheiss <61433811+andyeschultheiss@users.noreply.github.com>
2020-05-12 10:59:10 -0700

    Exception handling
    Use try/catch to customize error messaging

---
+++ Exception-Handling-Examples/Exception-Handling-Examples/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exception_Handling_Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numList = new List<int> {1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
+            try
+            {
+                Console.WriteLine("Please enter an integer:");
+                int inputNum = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Integer division of the first ten members of the Fibonacci sequence by " + inputNum + " yields:");
+                foreach (int num in numList)
+                {
+                    Console.WriteLine(Convert.ToString(num / inputNum));
+                }
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter a whole number");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by zero");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Integer division attempted.");
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
