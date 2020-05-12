89e00a843c678150ca5078fd4781a69aae6785e2
andyeschultheiss <61433811+andyeschultheiss@users.noreply.github.com>
2020-05-12 15:13:11 -0700

    Overloading examples
    demo of overloading methods

---
+++ Method-Overloading-Example/Method-Overloading-Example/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Method_Overloading_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program demonstrates method overloading.");
+            Console.WriteLine("Please enter an integer:");
+            int inputNum = Convert.ToInt32(Console.ReadLine());
+            int outputNum = mathOps.addTwo(inputNum);
+            Console.WriteLine(inputNum + " plus 2 equals " + outputNum);
+
+            Console.WriteLine("Now, enter a decimal number:");
+            decimal decimalInput = Convert.ToDecimal(Console.ReadLine());
+            outputNum = mathOps.addFour(decimalInput);
+            Console.WriteLine(decimalInput + " plus 4 equals " + outputNum + " (in integer math)");
+
+            Console.WriteLine("Now, enter a string:");
+            string stringInput = Console.ReadLine();
+            outputNum = mathOps.addSix(stringInput);
+            Console.WriteLine(stringInput + " plus 6 equals " + outputNum);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Method-Overloading-Example/Method-Overloading-Example/mathOps.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Method_Overloading_Example
+{
+    class mathOps
+    {
+        public static int addTwo(int inputInt)
+        {
+            return inputInt + 2;
+        }
+
+        public static int addFour(decimal inputDec)
+        {
+            return Convert.ToInt32(inputDec) + 4;
+        }
+
+        public static int addSix(string inputString)
+        {
+            return Convert.ToInt32(inputString) + 6;
+        }
+
+        public static int subtractFive(int inputInt)
+        {
+            return inputInt - 5;
+        }
+    }
+}
