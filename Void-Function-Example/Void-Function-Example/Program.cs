7120652daf6b6de38e8587cfd3c2aeea927bd788
andyeschultheiss <61433811+andyeschultheiss@users.noreply.github.com>
2020-05-13 11:16:03 -0700

    void function demo
    show use of void function

---
+++ Void-Function-Example/Void-Function-Example/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Void_Function_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an integer:");
+            int numOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter another integer:");
+            int numTwo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The second number entered is:");
+            mathOps.squareAndDisplay(opNum: numOne, dispNum: numTwo);
+
+        }
+    }
+}
---
+++ Void-Function-Example/Void-Function-Example/mathOps.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Void_Function_Example
+{
+    class mathOps
+    {
+        public static void squareAndDisplay(int opNum, int dispNum)
+        {
+            opNum = opNum * opNum;
+            Console.WriteLine(dispNum);
+        
+        }
+    }
+}
