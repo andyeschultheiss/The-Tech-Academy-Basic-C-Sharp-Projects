4cc95b6ac370a6a8edd676fd1b64559c7b87dfb5
andyeschultheiss <61433811+andyeschultheiss@users.noreply.github.com>
2020-05-19 13:48:22 -0700

    lambda expressions
    using lambda expressions and comparing with foreach loop

---
+++ Lambda-Expression-Example/Lambda-Expression-Example/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lambda_Expression_Example
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void SayName()
+        {
+            Console.WriteLine("Name: {0} {1}", FirstName, LastName);
+        }
+
+        public void ShowID()
+        {
+            Console.WriteLine("ID: {0}", Id);
+        }
+
+        public static bool operator ==(Employee firstEmployee, Employee secondEmployee)
+        {
+            if (firstEmployee.Id == secondEmployee.Id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator !=(Employee firstEmployee, Employee secondEmployee)
+        {
+            if (firstEmployee.Id != secondEmployee.Id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
