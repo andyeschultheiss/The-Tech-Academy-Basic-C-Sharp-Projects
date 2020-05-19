ce1a957ec50b3712d5edf56b866370dd18224376
andyeschultheiss <61433811+andyeschultheiss@users.noreply.github.com>
2020-05-18 18:26:14 -0700

    struct example
    demo of struct usage

---
+++ Struct-Example/Struct-Example/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Struct_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number newNumber = new Number();
+            newNumber.Amount = 3.14159m;
+            Console.WriteLine(newNumber.Amount);
+            Console.ReadLine();
+        }
+
+        public struct Number
+        {
+            public decimal Amount { get; set; }
+
+        }
+    }
+}
