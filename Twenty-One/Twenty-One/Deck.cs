df497c6281a7069d69a03283a2001bdc5fb5ad38
andyeschultheiss <61433811+andyeschultheiss@users.noreply.github.com>
2020-05-18 17:32:20 -0700

    enum practice
    days of the week example with dictionary and try/catch to validate user input

--- Twenty-One/Twenty-One/Deck.cs
+++ Twenty-One/Twenty-One/Deck.cs
@@ -11,24 +11,20 @@ namespace Twenty_One
         public Deck()
         {
             Cards = new List<Card>();
-            List<string> Suits = new List<string>() { "Clubs", "Diamonds", "Hearts", "Spades" };
-            List<string> Faces = new List<string>()
+
+            for (int i = 0; i < 13; i++)
             {
-                "Two", "Three", "Four", "Five", "Six", "Seven",
-                "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"
-            };
-            foreach (string suit in Suits)
-            {
-                foreach (string face in Faces)
+                for (int j = 0; j < 4; j++)
                 {
                     Card card = new Card();
-                    card.Suit = suit;
-                    card.Face = face;
+                    card.Face = (Face)i;
+                    card.Suit = (Suit)j;
                     Cards.Add(card);
                 }
             }
 
         }
+
         public List<Card> Cards { get; set; }
 
         public void Shuffle(int times = 1)
