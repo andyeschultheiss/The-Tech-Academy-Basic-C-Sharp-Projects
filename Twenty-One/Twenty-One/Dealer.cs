a84ff417525669b5328296fc596e1d905369709e
andyeschultheiss <61433811+andyeschultheiss@users.noreply.github.com>
2020-05-20 16:19:48 -0700

    Simple console BlackJack
    console app demonstrating understanding of classes, public and private, lambda expressions, dictionaries, and other logic operations

--- Twenty-One/Twenty-One/Dealer.cs
+++ Twenty-One/Twenty-One/Dealer.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Twenty_One
 {
-    class Dealer
+    public class Dealer
     {
         public string Name { get; set; }
         public Deck Deck { get; set; }
--- Twenty-One/Twenty-One/Program.cs
+++ Twenty-One/Twenty-One/Program.cs
@@ -11,22 +11,28 @@ namespace Twenty_One
         static void Main(string[] args)
         {
             Game game = new TwentyOneGame();
-            game.Players = new List<Player>();
-            Player player = new Player();
-            player.Name = "Bilbo";
-            game += player;
-            game.Name = "BlackJack";
-
-            Deck deck = new Deck();
-        
-            deck.Shuffle(3);
-
-            foreach (Card card in deck.Cards)
+            Console.WriteLine("Welcome to the Blackjack table. How many players today?");
+            int numberOfPlayers = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < numberOfPlayers; i++)
             {
-                Console.WriteLine(card.Face + " of " + card.Suit);
+                Console.WriteLine("Please enter your name:");
+                string playerName = Console.ReadLine();
+                Console.WriteLine("How much money would you like to put up?");
+                int bank = Convert.ToInt32(Console.ReadLine());
+                Player newPlayer = new Player(playerName, bank);
+                game += newPlayer;
+                newPlayer.IsActive = true;
             }
-           
-            Console.ReadLine();
+
+            while (game.Players.Count > 0 && game.Players.Sum(x => x.Balance) > 0)
+            {
+                game.Play();
+            }
+
+            
+            Console.WriteLine("Thank you for playing. Come again soon!");
+            Console.Read();
+
         }
 
        
