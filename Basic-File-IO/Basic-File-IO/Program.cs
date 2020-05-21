ab42005b5d8fa8c218f463fe03c4914ccef806cd
andyeschultheiss <61433811+andyeschultheiss@users.noreply.github.com>
2020-05-21 10:14:03 -0700

    console file logger and reader
    basic logging to txt file and reading back to console

---
+++ Basic-File-IO/Basic-File-IO/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Basic_File_IO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Please enter a number:");
+            string inputNum = Console.ReadLine();
+            string filePath = @"C:\Users\public\logs\numlog.txt";
+            string returnText;
+            using (StreamWriter file = new StreamWriter(filePath, true))
+            {
+                file.WriteLine(inputNum);
+            }
+            using (StreamReader file = new StreamReader(filePath, true))
+            {
+                returnText = file.ReadToEnd().ToString();
+            }
+
+            Console.WriteLine("Number logged to: {0}", filePath);
+            Console.WriteLine("File contents:");
+            Console.WriteLine(returnText);
+
+            Console.Read();
+        }
+    }
+}
